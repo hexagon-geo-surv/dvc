hexagon-geo-surv/dvc

commit b14a972cd057a5fcf6dfbee5da475e30c1f36bec

repofs: use / as root_marker
This makes dvcfs/repofs much easier to work with and allows it to support
isabs/abspath/etc correctly, as previously any full path (with root_marker "")
would be impossible to distinguish from a relative one.

This also detaches external workspaces from the main repo workspace in
DvcFileSystem, which was necessary to be able to distinguish dvcfs abspaths
from local external output paths. Note that currently we still have limited
support for those (e.g. get/api supports them, but import doesn't) and this
change keeps it that way.

dvc/repo/metrics/show.py
@@ -24,11 +24,11 @@ def _to_fs_paths(metrics: List[Output]) -> StrPaths:
     result = []
     for out in metrics:
         if out.metric:
-            result.append(out.fs_path)
+            result.append(out.repo.repo_fs.from_os_path(out.fs_path))
         elif out.live:
             fs_path = summary_fs_path(out)
             if fs_path:
-                result.append(fs_path)
+                result.append(out.repo.repo_fs.from_os_path(fs_path))
     return result
 
 
@@ -78,12 +78,16 @@ def _read_metric(path, fs, rev, **kwargs):
 def _read_metrics(repo, metrics, rev, onerror=None):
     fs = RepoFileSystem(repo=repo)
 
+    relpath = ""
+    if repo.root_dir != repo.fs.path.getcwd():
+        relpath = repo.fs.path.relpath(repo.root_dir, repo.fs.path.getcwd())
+
     res = {}
     for metric in metrics:
         if not fs.isfile(metric):
             continue
 
-        res[os.path.relpath(metric, os.getcwd())] = _read_metric(
+        res[os.path.join(relpath, *fs.path.parts(metric))] = _read_metric(
             metric, fs, rev, onerror=onerror
         )
 

dvc/api.py
@@ -19,8 +19,9 @@ def get_url(path, repo=None, rev=None, remote=None):
     directory in the remote storage.
     """
     with Repo.open(repo, rev=rev, subrepos=True, uninitialized=True) as _repo:
+        fs_path = _repo.repo_fs.from_os_path(path)
         with reraise(FileNotFoundError, PathMissingError(path, repo)):
-            info = _repo.repo_fs.info(path)
+            info = _repo.repo_fs.info(fs_path)
 
         dvc_info = info.get("dvc_info")
         if not dvc_info:

dvc/fs/base.py
@@ -83,7 +83,10 @@ class FileSystem:
     def path(self):
         from .path import Path
 
-        return Path(self.sep)
+        def _getcwd():
+            return self.fs.root_marker
+
+        return Path(self.sep, getcwd=_getcwd)
 
     @classmethod
     def _strip_protocol(cls, path: str) -> str:

dvc/fs/dvc.py
@@ -1,5 +1,4 @@
 import logging
-import os
 import threading
 import typing
 
@@ -22,31 +21,43 @@ class _DvcFileSystem(AbstractFileSystem):  # pylint:disable=abstract-method
         repo: DVC repo.
     """
 
-    def __init__(self, **kwargs):
+    root_marker = "/"
+
+    def __init__(self, workspace=None, **kwargs):
         super().__init__(**kwargs)
         self.repo = kwargs["repo"]
+        self.workspace = workspace or "repo"
+
+    @cached_property
+    def path(self):
+        from .path import Path
+
+        def _getcwd():
+            return self.root_marker
+
+        return Path(self.sep, getcwd=_getcwd)
 
     @property
     def config(self):
         raise NotImplementedError
 
     def _get_key(self, path):
-        from dvc.fs.local import LocalFileSystem
+        if self.workspace != "repo":
+            from . import get_cloud_fs
 
-        from . import get_cloud_fs
-
-        cls, kwargs, fs_path = get_cloud_fs(None, url=path)
-
-        if cls != LocalFileSystem or os.path.isabs(path):
+            cls, kwargs, fs_path = get_cloud_fs(None, url=path)
             fs = cls(**kwargs)
-            return (cls.scheme, *fs.path.parts(fs_path))
+            return (self.workspace, *fs.path.parts(fs_path))
 
-        fs_key = "repo"
-        key = path.split(self.sep)
-        if key == ["."] or key == [""]:
+        path = self.path.abspath(path)
+        if path == self.root_marker:
+            return (self.workspace,)
+
+        key = self.path.relparts(path, self.root_marker)
+        if key == (".") or key == (""):
             key = ()
 
-        return (fs_key, *key)
+        return (self.workspace, *key)
 
     def _get_fs_path(self, path: "AnyPath", remote=None):
         from dvc.config import NoRemoteError

dvc/fs/git.py
@@ -40,6 +40,12 @@ class GitFileSystem(FileSystem):  # pylint:disable=abstract-method
             }
         )
 
+    @cached_property
+    def path(self):
+        from .path import Path
+
+        return Path(self.sep, getcwd=os.getcwd)
+
     @wrap_prop(threading.Lock())
     @cached_property
     def fs(self) -> "FsspecGitFileSystem":

dvc/fs/path.py
@@ -105,7 +105,11 @@ class Path:
         return self.isin_or_eq(left, right) or self.isin(right, left)
 
     def relpath(self, path, start):
-        return self.flavour.relpath(path, start=start)
+        if not start:
+            start = "."
+        return self.flavour.relpath(
+            self.abspath(path), start=self.abspath(start)
+        )
 
     def relparts(self, path, base):
         return self.parts(self.relpath(path, base))

dvc/fs/repo.py
@@ -73,6 +73,8 @@ class _RepoFileSystem(AbstractFileSystem):  # pylint:disable=abstract-method
         kwargs: Additional keyword arguments passed to the `DvcFileSystem()`.
     """
 
+    root_marker = "/"
+
     PARAM_REPO_URL = "repo_url"
     PARAM_REPO_ROOT = "repo_root"
     PARAM_REV = "rev"
@@ -103,7 +105,10 @@ class _RepoFileSystem(AbstractFileSystem):  # pylint:disable=abstract-method
         else:
             self.repo_factory = repo_factory
 
-        self.path = Path(self.sep)
+        def _getcwd():
+            return self.root_marker
+
+        self.path = Path(self.sep, getcwd=_getcwd)
         self.repo = repo
         self.hash_jobs = repo.fs.hash_jobs
         self._traverse_subrepos = subrepos
@@ -258,17 +263,7 @@ class _RepoFileSystem(AbstractFileSystem):  # pylint:disable=abstract-method
         """
         Returns a pair of fss based on repo the path falls in, using prefix.
         """
-        from dvc.utils import as_posix
-
-        if os.path.isabs(path):
-            if self.repo.fs.path.isin_or_eq(path, self.repo.root_dir):
-                path = self.repo.fs.path.relpath(path, self.repo.root_dir)
-            else:
-                return None, None, self.repo.dvcfs, path
-
-        path = as_posix(path)
-
-        parts = self.path.parts(path)
+        parts = self.path.relparts(path, self.root_marker)
         if parts and parts[0] == os.curdir:
             parts = parts[1:]
 
@@ -461,3 +456,11 @@ class RepoFileSystem(FileSystem):
     @property
     def config(self):
         return self.fs.config
+
+    def from_os_path(self, path):
+        from dvc.utils import as_posix
+
+        if os.path.isabs(path):
+            path = os.path.relpath(path, self.repo.root_dir)
+
+        return as_posix(path)

dvc/repo/__init__.py
@@ -491,12 +491,22 @@ class Repo:
     @contextmanager
     def open_by_relpath(self, path, remote=None, mode="r", encoding=None):
         """Opens a specified resource as a file descriptor"""
+        from dvc.fs.dvc import DvcFileSystem
         from dvc.fs.repo import RepoFileSystem
 
-        fs = RepoFileSystem(repo=self, subrepos=True)
+        if os.path.isabs(path):
+            fs = DvcFileSystem(repo=self, workspace="local")
+            fs_path = path
+        else:
+            fs = RepoFileSystem(repo=self, subrepos=True)
+            fs_path = fs.from_os_path(path)
+
         try:
             with fs.open(
-                path, mode=mode, encoding=encoding, remote=remote
+                fs_path,
+                mode=mode,
+                encoding=encoding,
+                remote=remote,
             ) as fobj:
                 yield fobj
         except FileNotFoundError as exc:

commit fb5ac280a658b2b1fe691d191075b7df7b8115fc

metrics: show: Fix show from subdir.
This was a regression introduced in #7712.
Calling `repo.metrics.show` from a subdir stopped returning metrics because `fs.isfile(metric)` check returned false.

dvc/repo/metrics/show.py
@@ -1,5 +1,4 @@
 import logging
-import os
 from typing import List
 
 from scmrepo.exceptions import SCMError
@@ -85,12 +84,14 @@ def _read_metrics(repo, metrics, rev, onerror=None):
 
     res = {}
     for metric in metrics:
+        rel_metric_path = fs.path.join(relpath, *fs.path.parts(metric))
         if not fs.isfile(metric):
-            continue
+            if fs.isfile(rel_metric_path):
+                metric = rel_metric_path
+            else:
+                continue
 
-        res[os.path.join(relpath, *fs.path.parts(metric))] = _read_metric(
-            metric, fs, rev, onerror=onerror
-        )
+        res[rel_metric_path] = _read_metric(metric, fs, rev, onerror=onerror)
 
     return res
 

tests/func/metrics/test_show.py
@@ -21,6 +21,32 @@ def test_show_simple(tmp_dir, dvc, run_copy_metrics):
     }
 
 
+def test_show_simple_from_subdir(tmp_dir, dvc, run_copy_metrics):
+    subdir = tmp_dir / "subdir"
+    subdir.mkdir()
+    tmp_dir.gen("metrics_t.yaml", "1.1")
+    run_copy_metrics(
+        "metrics_t.yaml",
+        "subdir/metrics.yaml",
+        metrics=["subdir/metrics.yaml"],
+    )
+
+    expected_path = os.path.join("subdir", "metrics.yaml")
+    assert dvc.metrics.show() == {"": {"data": {expected_path: {"data": 1.1}}}}
+
+    expected_path = os.path.join("..", "subdir", "metrics.yaml")
+    with subdir.chdir():
+        assert dvc.metrics.show() == {
+            "": {"data": {expected_path: {"data": 1.1}}}
+        }
+    subdir2 = tmp_dir / "subdir2"
+    subdir2.mkdir()
+    with subdir2.chdir():
+        assert dvc.metrics.show() == {
+            "": {"data": {expected_path: {"data": 1.1}}}
+        }
+
+
 def test_show(tmp_dir, dvc, run_copy_metrics):
     tmp_dir.gen("metrics_t.yaml", "foo: 1.1")
     run_copy_metrics(